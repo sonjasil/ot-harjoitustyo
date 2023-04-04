441fb07563612058f7d708105801cf3a72908e8f
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-04-04 20:28:43 +0300

    Added tasks for testing

--- tasks.py
+++ tasks.py
@@ -5,9 +5,14 @@ def start(ctx):
     ctx.run("python3 src/app/draw_grid.py", pty=True)
 
 @task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+
