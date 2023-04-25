7e0d1fa52bf9ca244166995e2ce125f6638f509b
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-04-25 20:58:57 +0300

    Added sprite for middle layer

---
+++ src/sprites/middle.py
@@ -0,0 +1,15 @@
+import os
+import pygame
+
+dirname = os.path.dirname(__file__)
+
+class Middle(pygame.sprite.Sprite):
+    def __init__(self, moon_x=0, moon_y=0):
+        super().__init__()
+
+        self.image = pygame.image.load(os.path.join(dirname, "..", "graphics", "middle.png"))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = moon_x
+        self.rect.y = moon_y
