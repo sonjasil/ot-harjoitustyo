b26e118431bc1fcb49bfe61c7070f83db069fa35
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-04-25 18:54:01 +0300

    Added new tests

--- src/tests/grid_size_test.py
+++ src/tests/grid_size_test.py
@@ -1,11 +1,21 @@
 import unittest
-from app.grid import Grid
-from app.draw_grid import create_map 
+from grid import Grid
+from draw_grid import create_map 
 
 class TestGrid(unittest.TestCase):
     def setUp(self):
-        map = create_map(1)
-        self.grid = Grid(map, 200)
+        map1 = create_map(1)
+        map2 = create_map(2)
+        map3 = create_map(3)
+        self.grid1 = Grid(map1, 200)
+        self.grid2 = Grid(map2, 200)
+        self.grid3 = Grid(map3, 200)
 
-    def test_grid_size(self):
-        self.assertEqual(self.grid.size, 3)
+    def test_grid_size1(self):
+        self.assertEqual(self.grid1.size, 3)
+
+    def test_grid_size2(self):
+        self.assertEqual(self.grid2.size, 4)
+
+    def test_grid_size3(self):
+        self.assertEqual(self.grid3.size, 5)
