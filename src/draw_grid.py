79a67966e86cde135be0382f8741028b8d65cfa8
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-04-25 21:03:25 +0300

    Added images and middle layer into game

--- src/draw_grid.py
+++ src/draw_grid.py
@@ -1,9 +1,11 @@
 import pygame
-from grid import Grid
+from random import shuffle
+from grid import Grid, Layer
+from matching_game import Game
 
 def create_map(level):
     if level == 1:
-        map_grid = [[(0, 1), (0, 2), (0, 3), (0,4)],
+        map_grid = [[(0, 1), (0, 2), (0, 3), (0, 4)],
                     [(0, 5), (0, 6), (0, 2), (0, 3)],
                     [(0, 5), (0, 6), (0, 1), (0, 4)]]
     elif level == 2:
@@ -23,26 +25,40 @@ def create_map(level):
 CELL_SIZE = 200
 
 def main(level):
-    grid = create_map(level)
-    height = len(grid)
-    width = len(grid[0])
-    screen_height = height * CELL_SIZE
+    grid_map = create_map(level)
+    shuffle(grid_map)
+    for row in grid_map:
+        shuffle(row)
+    height = len(grid_map)
+    width = len(grid_map[0])
+    screen_height = height * CELL_SIZE + 20
     screen_width = width * CELL_SIZE
 
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     pygame.display.set_caption("Muistipeli")
 
-    grid = Grid(grid, CELL_SIZE)
+    grid = Grid(grid_map, CELL_SIZE)
+    middle = Layer(grid_map, CELL_SIZE)
+    game = Game(grid_map, CELL_SIZE)
 
     pygame.init()
 
+    game.all_sprites.draw(screen)
+    middle.all_sprites.draw(screen)
     grid.all_sprites.draw(screen)
 
     running = True
 
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = event.pos
+                for sprite in game.all_sprites:
+                    for mid in middle.all_sprites:
+                        if mid.rect.collidepoint(mouse_x, mouse_y) and sprite.rect.collidepoint(mouse_x, mouse_y):
+                            screen.blit(mid.image, mid.rect)
+                            screen.blit(sprite.image, sprite.rect)
             if event.type == pygame.QUIT:
                 running = False
 
