867253ab621f34da4a174e010bb8073d2e1bf3c9
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-04-25 21:02:46 +0300

    Added sprites for middle layer

--- src/grid.py
+++ src/grid.py
@@ -1,5 +1,6 @@
 import pygame
 from sprites.back import Back
+from sprites.middle import Middle
 
 class Grid:
     def __init__(self, grid_map, cell_size):
@@ -25,3 +26,28 @@ class Grid:
                     self.backs.add(Back(normalized_x, normalized_y))
 
             self.all_sprites.add(self.backs)
+
+class Layer:
+    def __init__(self, grid_map, cell_size):
+        self.grid_map = grid_map
+        self.cell_size = cell_size
+        self.size = len(grid_map)
+        self.middles = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.Group()
+
+        self._initialize_sprites(grid_map)
+
+    def _initialize_sprites(self, grid_map):
+        height = len(grid_map)
+        width = len(grid_map[0])
+
+        for middle_y in range(height):
+            for middle_x in range(width):
+                cell = self.grid_map[middle_y][middle_x]
+                normalized_x = middle_x * self.cell_size
+                normalized_y = middle_y * self.cell_size
+
+                if cell[0] == 0:
+                    self.middles.add(Middle(normalized_x, normalized_y))
+
+            self.all_sprites.add(self.middles)
