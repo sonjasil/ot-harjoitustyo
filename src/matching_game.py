457643bf371786a368b3d4f7916fb790c823e4a1
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-04-27 13:29:20 +0300

    Added remaining images to levels 2 and 3

--- src/matching_game.py
+++ src/matching_game.py
@@ -5,6 +5,10 @@ from sprites.heart import Heart
 from sprites.moon import Moon
 from sprites.star import Star
 from sprites.tulip import Tulip
+from sprites.carrot import Carrot
+from sprites.gem import Gem
+from sprites.potato import Potato
+from sprites.sun import Sun
 
 class Game:
     def __init__(self, grid_map, cell_size):
@@ -17,6 +21,10 @@ class Game:
         self.moons = pygame.sprite.Group()
         self.stars = pygame.sprite.Group()
         self.tulips = pygame.sprite.Group()
+        self.carrots = pygame.sprite.Group()
+        self.gems = pygame.sprite.Group()
+        self.potatoes = pygame.sprite.Group()
+        self.suns = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
 
         self._initialize_sprites(grid_map)
@@ -43,11 +51,24 @@ class Game:
                     self.stars.add(Star(normalized_x, normalized_y))
                 elif cell[1] == 6:
                     self.tulips.add(Tulip(normalized_x, normalized_y))
+                elif cell[1] == 7:
+                    self.carrots.add(Carrot(normalized_x, normalized_y))
+                elif cell[1] == 8:
+                    self.gems.add(Gem(normalized_x, normalized_y))
+                elif cell[1] == 9:
+                    self.potatoes.add(Potato(normalized_x, normalized_y))
+                elif cell[1] == 10:
+                    self.suns.add(Sun(normalized_x, normalized_y))
+
 
             self.all_sprites.add(self.apples,
                                  self.balloons,
                                  self.hearts,
                                  self.moons,
                                  self.stars,
-                                 self.tulips)
+                                 self.tulips,
+                                 self.carrots,
+                                 self.gems,
+                                 self.potatoes,
+                                 self.suns)
 
