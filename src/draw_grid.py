be8bf8a6479e2a6c3eafc5480f73fec1c2b87010
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-05-01 17:11:32 +0300

    Added automatic turning of cards and removal of pairs

--- src/draw_grid.py
+++ src/draw_grid.py
@@ -49,16 +49,52 @@ def main(level):
     grid.all_sprites.draw(screen)
 
     running = True
+    clicks = 0
+    cooldown = 3000
+    last = pygame.time.get_ticks()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos
-                for sprite in game.all_sprites:
-                    for mid in middle.all_sprites:
-                        if mid.rect.collidepoint(mouse_x, mouse_y) and sprite.rect.collidepoint(mouse_x, mouse_y):
-                            screen.blit(mid.image, mid.rect)
-                            screen.blit(sprite.image, sprite.rect)
+                if clicks == 0:
+                    for sprite in game.all_sprites:
+                        for mid in middle.all_sprites:
+                            for back in grid.all_sprites:
+                                if mid.rect.collidepoint(mouse_x, mouse_y) and sprite.rect.collidepoint(mid.rect.x, mid.rect.y):
+                                    if sprite.rect.collidepoint(back.rect.x, back.rect.y):
+                                        screen.blit(mid.image, mid.rect)
+                                        screen.blit(sprite.image, sprite.rect)
+                                        sprite1 = sprite
+                                        middle_card1 = mid
+                                        back1 = back
+                                        clicks += 1
+                elif clicks == 1:
+                    for sprite in game.all_sprites:
+                        for mid in middle.all_sprites:
+                            for back in grid.all_sprites:
+                                if mid.rect.collidepoint(mouse_x, mouse_y) and sprite.rect.collidepoint(mid.rect.x, mid.rect.y):
+                                    if sprite.rect.collidepoint(back.rect.x, back.rect.y):
+                                        screen.blit(mid.image, mid.rect)
+                                        screen.blit(sprite.image, sprite.rect)
+                                        sprite2 = sprite
+                                        middle_card2 = mid
+                                        clicks += 1
+                                        back2 = back
+            if clicks == 2:    
+                now = pygame.time.get_ticks()
+                if sprite1.id != sprite2.id and now - last >= cooldown:
+                    last = now
+                    screen.blit(middle_card1.image, middle_card1.rect)
+                    screen.blit(middle_card2.image, middle_card2.rect)
+                    screen.blit(back1.image, back1.rect)
+                    screen.blit(back2.image, back2.rect)
+                    clicks = 0
+                elif sprite1.id == sprite2.id and now - last >= cooldown:
+                    last = now
+                    screen.blit(middle_card1.image, middle_card1.rect)
+                    screen.blit(middle_card2.image, middle_card2.rect)
+                    clicks = 0
             if event.type == pygame.QUIT:
                 running = False
 
