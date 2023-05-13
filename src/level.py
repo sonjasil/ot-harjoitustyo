1e41d2b821c711839ab659b39611395c3fd31370
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-05-13 15:35:28 +0300

    Format fixes

---
+++ src/level.py
@@ -0,0 +1,56 @@
+from random import shuffle
+
+class LevelMap:
+    """ Luokka, joka luo tason kartan.
+    
+    Attributes:
+        level: tason numero, määrittää minkä pohjan mukaan kartta luodaan.
+    """
+
+    def __init__(self, level):
+        self.level = level
+        self.grid_map = self.create_map(self.level)
+        self.cell_size = 200
+        self.screen_height = None
+        self.screen_width = None
+
+        self.create_map(self.level)
+        self.initialize_map()
+
+    def create_map(self, level):
+        """Luo pelin ruudukon tason perusteella
+
+        Args:
+            level: Pelin taso.
+
+        Returns:
+            map_grid: Pelin taso taulukkona, jossa tuplejen numerot vastaavat spriteja.
+        """
+
+        if level == 1:
+            map_grid = [[(0, 1), (0, 2), (0, 3), (0, 4)],
+                        [(0, 5), (0, 6), (0, 2), (0, 3)],
+                        [(0, 5), (0, 6), (0, 1), (0, 4)]]
+        elif level == 2:
+            map_grid = [[(0, 8), (0, 6), (0, 4), (0, 2)],
+                        [(0, 7), (0, 7), (0, 5), (0, 1)],
+                        [(0, 3), (0, 3), (0, 1), (0, 6)],
+                        [(0, 8), (0, 4), (0, 5), (0, 2)]]
+        elif level == 3:
+            map_grid = [[(0, 1), (0, 10), (0, 2), (0, 9), (0, 5)],
+                        [(0, 1), (0, 8), (0, 7), (0, 6), (0, 5)],
+                        [(0, 6), (0, 4), (0, 7), (0, 3), (0, 3)],
+                        [(0, 10), (0, 2), (0, 8), (0, 9), (0, 4)]]
+        return map_grid
+
+    def initialize_map(self):
+        """Alustaa ruudukon ja näytön koon ruudukon mukaan.
+        """
+
+        shuffle(self.grid_map)
+        for row in self.grid_map:
+            shuffle(row)
+        height = len(self.grid_map)
+        width = len(self.grid_map[0])
+        self.screen_height = height * self.cell_size + 80
+        self.screen_width = width * self.cell_size
--- src/run_game.py
+++ src/run_game.py
@@ -1,8 +1,7 @@
-from random import shuffle
 import pygame
 from grid import Grid, Layer
 from matching_game import Game2
-
+from level import LevelMap
 
 class MatchingGame:
     """ Luokka, joka luo muistipelin tason mukaan.
@@ -17,54 +16,26 @@ class MatchingGame:
         Args:
             level: Pelin taso, määrittää ruudukon koon
         """
+        super().__init__()
+        self.lvl = level
+        self.level = LevelMap(self.lvl)
+        self.grid_map = self.level.grid_map
+        self.grid = Grid(self.grid_map, self.level.cell_size)
+        self.middle = Layer(self.grid_map, self.level.cell_size)
+        self.game = Game2(self.grid_map, self.level.cell_size)
+        self.last = None
+        self.clicks = 0
+        self.score = 0
+        self.box = pygame.Rect(0, self.level.screen_height -
+                               80, self.level.screen_width, 80)
+        self.mouse_x, self.mouse_y = 0, 0
 
-        self.level = level
-        self.grid_map = self.create_map(self.level)
-        self.CELL_SIZE = 200
-        self.grid = Grid(self.grid_map, self.CELL_SIZE)
-        self.middle = Layer(self.grid_map, self.CELL_SIZE)
-        self.game = Game2(self.grid_map, self.CELL_SIZE)
-
-    def create_map(self, level):
-        """Luo pelin ruudukon tason perusteella
-
-        Args:
-            level: Pelin taso.
-
-        Returns:
-            map_grid: Pelin taso taulukkona, jossa tuplejen numerot vastaavat spriteja.
-        """
-
-        if level == 1:
-            map_grid = [[(0, 1), (0, 2), (0, 3), (0, 4)],
-                        [(0, 5), (0, 6), (0, 2), (0, 3)],
-                        [(0, 5), (0, 6), (0, 1), (0, 4)]]
-        elif level == 2:
-            map_grid = [[(0, 8), (0, 6), (0, 4), (0, 2)],
-                        [(0, 7), (0, 7), (0, 5), (0, 1)],
-                        [(0, 3), (0, 3), (0, 1), (0, 6)],
-                        [(0, 8), (0, 4), (0, 5), (0, 2)]]
-        elif level == 3:
-            map_grid = [[(0, 1), (0, 10), (0, 2), (0, 9), (0, 5)],
-                        [(0, 1), (0, 8), (0, 7), (0, 6), (0, 5)],
-                        [(0, 6), (0, 4), (0, 7), (0, 3), (0, 3)],
-                        [(0, 10), (0, 2), (0, 8), (0, 9), (0, 4)]]
-        return map_grid
-
-    def initialize_map(self):
-        """Alustaa ruudukon ja näytön koon ruudukon mukaan.
-        """
-
-        shuffle(self.grid_map)
-        for row in self.grid_map:
-            shuffle(row)
-        height = len(self.grid_map)
-        width = len(self.grid_map[0])
-        self.screen_height = height * self.CELL_SIZE + 80
-        self.screen_width = width * self.CELL_SIZE
+        self.sprite1, self.sprite2 = None, None
+        self.middle_card1, self.middle_card2 = None, None
+        self.back1, self.back2 = None, None
 
         self.screen = pygame.display.set_mode(
-            (self.screen_width, self.screen_height))
+            (self.level.screen_width, self.level.screen_height))
         pygame.display.set_caption("Muistipeli")
 
     def run_game(self):
@@ -72,12 +43,11 @@ class MatchingGame:
         """
 
         pygame.init()
-        self.initialize_map()
-        self.set_elements()
+        running = True
         self.draw_sprites()
         self.last = pygame.time.get_ticks()
 
-        while self.running:
+        while running:
             self.render_text()
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -86,10 +56,11 @@ class MatchingGame:
                         self.click_1()
                     elif self.clicks == 1:
                         self.click_2()
+                        self.last = pygame.time.get_ticks()
                 if self.clicks == 2:
                     self.turn_cards()
                 if event.type == pygame.QUIT:
-                    self.running = False
+                    running = False
             pygame.display.flip()
         pygame.quit()
 
@@ -101,75 +72,68 @@ class MatchingGame:
         self.middle.all_sprites.draw(self.screen)
         self.grid.all_sprites.draw(self.screen)
 
-    def set_elements(self):
-        """Määrittää ohjelman suorituksen muuttujat.
-        """
-
-        self.running = True
-        self.clicks = 0
-        self.score = 0
-        self.cooldown = 3000
-        self.text_font = pygame.font.SysFont("Arial", 24)
-        self.box = pygame.Rect(0, self.screen_height -
-                               80, self.screen_width, 80)
-
     def render_text(self):
         """Piirtää tekstin ja pistelaskurin näytölle.
         """
-
-        text = self.text_font.render(
+        text_font = pygame.font.SysFont("Arial", 24)
+        text = text_font.render(
             f"Käännetyt parit: {self.score}", True, (255, 255, 255))
-        self.screen.blit(text, (self.screen_width // 2 +
-                         40, self.screen_height - 50))
+        self.screen.blit(text, (self.level.screen_width // 2 +
+                         40, self.level.screen_height - 50))
 
     def click_1(self):
         """Piirtää ensimmäisen käännetyn kortin.
         """
 
-        for sprite in self.game.all_sprites:
+        for img in self.game.all_sprites:
             for mid in self.middle.all_sprites:
                 for back in self.grid.all_sprites:
-                    if mid.rect.collidepoint(self.mouse_x, self.mouse_y) and sprite.rect.collidepoint(mid.rect.x, mid.rect.y):
-                        if sprite.rect.collidepoint(back.rect.x, back.rect.y):
-                            self.screen.blit(mid.image, mid.rect)
-                            self.screen.blit(sprite.image, sprite.rect)
-                            self.sprite1 = sprite
-                            self.middle_card1 = mid
-                            self.back1 = back
-                            self.clicks += 1
+                    if mid.rect.collidepoint(self.mouse_x, self.mouse_y
+                                             ) and img.rect.collidepoint(mid.rect.x, mid.rect.y
+                                             ) and img.rect.collidepoint(back.rect.x, back.rect.y):
+                        self.screen.blit(mid.image, mid.rect)
+                        self.screen.blit(img.image, img.rect)
+                        self.sprite1 = img
+                        self.middle_card1 = mid
+                        self.back1 = back
+                        self.clicks += 1
 
     def click_2(self):
         """Piirtää toisen käännetyn kortin ruudulle. Lisää pistelaskurin summaa.
         """
 
-        self.score += 1
         pygame.draw.rect(self.screen, (0, 0, 0), self.box)
-        for sprite in self.game.all_sprites:
+        for img in self.game.all_sprites:
             for mid in self.middle.all_sprites:
                 for back in self.grid.all_sprites:
-                    if mid.rect.collidepoint(self.mouse_x, self.mouse_y) and sprite.rect.collidepoint(mid.rect.x, mid.rect.y):
-                        if sprite.rect.collidepoint(back.rect.x, back.rect.y):
-                            self.screen.blit(mid.image, mid.rect)
-                            self.screen.blit(sprite.image, sprite.rect)
-                            self.sprite2 = sprite
-                            self.middle_card2 = mid
-                            self.clicks += 1
-                            self.back2 = back
+                    if mid.rect.collidepoint(self.mouse_x, self.mouse_y
+                                             ) and img.rect.collidepoint(mid.rect.x, mid.rect.y
+                                             ) and img.rect.collidepoint(back.rect.x, back.rect.y):
+                        self.screen.blit(mid.image, mid.rect)
+                        self.screen.blit(img.image, img.rect)
+                        self.sprite2 = img
+                        self.middle_card2 = mid
+                        self.back2 = back
+        if self.sprite1.rect != self.sprite2.rect:
+            self.clicks += 1
+            self.score += 1
 
     def turn_cards(self):
-        """Kääntää käännetyt kortit takaisin ympäri, jos ne eivät ole pari tai poistaa kortit, jos ne ovat pari. 
+        """Kääntää käännetyt kortit takaisin ympäri, 
+            jos ne eivät ole pari tai poistaa kortit, jos ne ovat pari. 
             Poistaa poistettujen korttien paikat käytöstä.
         """
 
         now = pygame.time.get_ticks()
-        if self.sprite1.id != self.sprite2.id and now - self.last >= self.cooldown:
+        cooldown = 2500
+        if self.sprite1.id != self.sprite2.id and now - self.last >= cooldown:
             self.last = now
             self.screen.blit(self.middle_card1.image, self.middle_card1.rect)
             self.screen.blit(self.middle_card2.image, self.middle_card2.rect)
             self.screen.blit(self.back1.image, self.back1.rect)
             self.screen.blit(self.back2.image, self.back2.rect)
             self.clicks = 0
-        elif self.sprite1.id == self.sprite2.id and now - self.last >= self.cooldown:
+        elif self.sprite1.id == self.sprite2.id and now - self.last >= cooldown:
             self.last = now
             self.screen.blit(self.middle_card1.image, self.middle_card1.rect)
             self.screen.blit(self.middle_card2.image, self.middle_card2.rect)
