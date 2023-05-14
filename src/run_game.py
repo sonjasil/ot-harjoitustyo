18df524bf8071ce65849c83ac549908e9715180e
Sonja Silvonen <sonja.silvonen@outlook.com>
2023-05-14 17:33:00 +0300

    Added methods for saving and reading player scores

--- src/run_game.py
+++ src/run_game.py
@@ -1,7 +1,9 @@
+import pickle
 import pygame
 from grid import Grid, Layer
 from matching_game import Game2
 from level import LevelMap
+
 
 class MatchingGame:
     """ Luokka, joka luo muistipelin tason mukaan.
@@ -16,7 +18,7 @@ class MatchingGame:
         Args:
             level: Pelin taso, määrittää ruudukon koon
         """
-        super().__init__()
+
         self.lvl = level
         self.level = LevelMap(self.lvl)
         self.level.initialize_map()
@@ -25,8 +27,8 @@ class MatchingGame:
         self.middle = Layer(self.grid_map, self.level.cell_size)
         self.game = Game2(self.grid_map, self.level.cell_size)
         self.last = None
-        self.clicks = 0
-        self.score = 0
+        self.clicks, self.score = 0, 0
+        self.scores = self.get_scores()
         self.box = pygame.Rect(0, self.level.screen_height -
                                80, self.level.screen_width, 80)
         self.mouse_x, self.mouse_y = 0, 0
@@ -61,6 +63,7 @@ class MatchingGame:
                 if self.clicks == 2:
                     self.turn_cards()
                 if event.type == pygame.QUIT:
+                    self.load_scores()
                     running = False
             pygame.display.flip()
         pygame.quit()
@@ -77,10 +80,58 @@ class MatchingGame:
         """Piirtää tekstin ja pistelaskurin näytölle.
         """
         text_font = pygame.font.SysFont("Arial", 24)
-        text = text_font.render(
+        highscore = self.get_highscore()
+        text1 = text_font.render(
             f"Käännetyt parit: {self.score}", True, (255, 255, 255))
-        self.screen.blit(text, (self.level.screen_width // 2 +
+        text2 = text_font.render(
+            f"Paras tulos: {highscore}", True, (255, 255, 255))
+        self.screen.blit(text1, (self.level.screen_width // 2 +
                          40, self.level.screen_height - 50))
+        self.screen.blit(text2, (20, self.level.screen_height - 50))
+
+    def get_scores(self):
+        """Hakee pelaajan kaikki tulokset tiedostosta.
+        """
+
+        try:
+            if self.lvl == 1:
+                file = open("scores.p", "rb")
+            elif self.lvl == 2:
+                file = open("scores2.p", "rb")
+            elif self.lvl == 3:
+                file = open("scores3.p", "rb")
+            scores = pickle.load(file)
+            file.close()
+            return list(scores)
+        except:
+            return list()
+
+    def get_highscore(self):
+        """Hakee pelaajan parhaan tuloksen listasta, jos pelaajalla on tuloksia
+        """
+
+        try:
+            return min(self.scores)
+        except:
+            return "-"
+
+    def load_scores(self):
+        if self.lvl == 1 and self.score >= 6:
+            self.scores.append(self.score)
+        elif self.lvl == 2 and self.score >= 8:
+            self.scores.append(self.score)
+        elif self.lvl == 3 and self.score >= 10:
+            self.scores.append(self.score)
+
+        if self.lvl == 1:
+            file = open("scores.p", "wb")
+        elif self.lvl == 2:
+            file = open("scores2.p", "wb")
+        elif self.lvl == 3:
+            file = open("scores3.p", "wb")
+
+        pickle.dump(self.scores, file)
+        file.close()
 
     def click_1(self):
         """Piirtää ensimmäisen käännetyn kortin.
@@ -91,7 +142,7 @@ class MatchingGame:
                 for back in self.grid.all_sprites:
                     if mid.rect.collidepoint(self.mouse_x, self.mouse_y
                                              ) and img.rect.collidepoint(mid.rect.x, mid.rect.y
-                                             ) and img.rect.collidepoint(back.rect.x, back.rect.y):
+                                                                         ) and img.rect.collidepoint(back.rect.x, back.rect.y):
                         self.screen.blit(mid.image, mid.rect)
                         self.screen.blit(img.image, img.rect)
                         self.sprite1 = img
@@ -109,7 +160,7 @@ class MatchingGame:
                 for back in self.grid.all_sprites:
                     if mid.rect.collidepoint(self.mouse_x, self.mouse_y
                                              ) and img.rect.collidepoint(mid.rect.x, mid.rect.y
-                                             ) and img.rect.collidepoint(back.rect.x, back.rect.y):
+                                                                         ) and img.rect.collidepoint(back.rect.x, back.rect.y):
                         self.screen.blit(mid.image, mid.rect)
                         self.screen.blit(img.image, img.rect)
                         self.sprite2 = img
